d61c82c17f4a7dd6ad05e635e62e4304ac99bead
emrebboyuk <bboyuk.emre@gmail.com>
2024-05-24 08:25:26 +0300

    minor

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ menu = """Please select one of the following options:
 3) View all movies
 4) Watch a movie
 5) View watched movies.
-6) Exit.
+6) Add user to the app.
+7) Exit.
 
 Your selection: """
 welcome = "Welcome to the watchlist app!"
@@ -29,25 +30,30 @@ def prompt_add_movie():
 def print_movie_list(heading, movies):
     print(f"--{heading} movies--")
     for movie in movies:
-        movie_date = datetime.datetime.fromtimestamp(movie[1])
+        movie_date = datetime.datetime.fromtimestamp(movie[2])
         human_date = movie_date.strftime("%b %d %Y")
-        print(f"{movie[0]} (on {human_date})")
-    print("----\n")
-
-def print_watched_movie_list(username, movies):
-    print(f"--{username}'s watched movies--")
-    for movie in movies:
-        print(f"{movie[1]}")
+        print(f"{movie[0]}: {movie[1]} (on {human_date})")
     print("----\n")
 
 
 def prompt_watch_movie():
     username = input("Username: ")
-    movie_title = input("Enter movie title you've watched: ")
-    database.watch_movie(username, movie_title)
+    movie_id = input("Movie ID: ")
+    database.watch_movie(username, movie_id)
 
+def prompt_show_watched_movies():
+    username = input("Username: ")
+    movies = database.get_watched_movies(username)
+    if movies:
+        print_movie_list("Watched", movies)
+    else:
+        print("That user has watched no movies yet!")
 
-while (user_input := input(menu)) != "6":
+def prompt_add_user():
+    username = input("Username: ")
+    database.add_user(username)
+
+while (user_input := input(menu)) != "7":
     if user_input == "1":
         prompt_add_movie()
     elif user_input == "2":
@@ -59,8 +65,8 @@ while (user_input := input(menu)) != "6":
     elif user_input == "4":
         prompt_watch_movie()
     elif user_input == "5":
-        username = input("Username: ")
-        movies = database.get_watched_movies(username)
-        print_watched_movie_list(username, movies)
+        prompt_show_watched_movies()
+    elif user_input == "6":
+        prompt_add_user()
     else:
         print("Invalid input, please try again!")
